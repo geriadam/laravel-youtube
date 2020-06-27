3196b73527d2e656abcd38cd3a7eba98249a41f1
geri <geriadam1@gmail.com>
2020-06-28 00:56:03 +0700

    searching videos and channels

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,10 +13,73 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    
+                    <form action="">
+                        <input type="text" name="search" class="form-control" placeholder="Search Videos and Channel">
+                    </form>
                 </div>
             </div>
+            @if($channels->count() != 0)
+                <div class="card mt-5">
+                    <div class="card-header">
+                        Channels
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <th>Name</th>
+                                <th>Link</th>
+                            </thead>
+                            <tbody>
+                                @foreach($channels as $channel)
+                                    <tr>
+                                        <td>{{ $channel->name }}</td>
+                                        <td>
+                                            <a class="btn btn-sm btn-info" href="{{ route('channels.show', $channel->id) }}">
+                                                View Channel
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="row justify-content-center">
+                            {!! $channels->append(request()->query())->links() !!}
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+            @if($videos->count() != 0)
+                <div class="card mt-5">
+                    <div class="card-header">
+                        Videos
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <th>Title</th>
+                                <th>Link</th>
+                            </thead>
+                            <tbody>
+                                @foreach($videos as $video)
+                                    <tr>
+                                        <td>{{ $video->title }}</td>
+                                        <td>
+                                            <a class="btn btn-sm btn-info" href="{{ route('video.show', $video->id) }}">
+                                                View Video
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="row justify-content-center">
+                            {!! $videos->append(request()->query())->links() !!}
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
